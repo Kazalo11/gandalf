d47f685b847242e8ebe3db7df05ad5f45757308f
Kazal Oshodi <kazoshodi@gmail.com>
2024-11-19 22:58:16 +0000

    initalise game

--- internals/game.go
+++ internals/game.go
@@ -9,12 +9,15 @@ type Game struct {
 	Rounds  []models.Round
 }
 
-func (g *Game) InitGame(numOfPlayers int) {
+func InitGame(numOfPlayers int) Game {
+	g := Game{}
 	g.Deck = models.InitDeck()
+	g.Discard = models.Discard{}
 	for i := 0; i < numOfPlayers; i++ {
 		player := models.InitPlayer("Kazal")
 		player.DrawCards(&g.Deck, 4)
 		g.Players = append(g.Players, player)
 	}
+	return g
 
 }
