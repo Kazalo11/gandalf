be8666331a07cae899b5bdb2d62500d6223caa6f
Kazal Oshodi <kazoshodi@gmail.com>
2024-11-20 22:54:45 +0000

    starting server connection

--- server/subscribeHandler.go
+++ server/subscribeHandler.go
@@ -3,17 +3,26 @@ package server
 import (
 	"encoding/json"
 	"log"
+
+	"github.com/google/uuid"
 )
 
-type Message struct {
-	Action string `json:"action"`
-	Data   string `json:"data"`
+type SubscribeMessage struct {
+	Action string  `json:"action"`
+	ID     *string `json:"id,omitempty"`
 }
 
 func handleMessage(msg []byte) {
-	var parsedMsg Message
+	var parsedMsg SubscribeMessage
 	if err := json.Unmarshal(msg, &parsedMsg); err != nil {
 		log.Printf("Failed to parse message: %v", err)
 		return
 	}
+
+	action, id := parsedMsg.Action, parsedMsg.ID
+
+	if id != nil {
+		ID := uuid.MustParse(*id)
+	}
+
 }
