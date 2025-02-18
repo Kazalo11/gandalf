acc634708d2635ab94d7ce81e74a7444e7220018
Kazalo11 <kazoshodi@gmail.com>
2025-02-18 20:39:32 +0000

    parse message

--- server/client.go
+++ server/client.go
@@ -85,21 +85,8 @@ func (c *Client) receiveMessages() {
 				return
 			}
 
-			w, err := c.conn.NextWriter(websocket.TextMessage)
-			if err != nil {
-				return
-			}
-			w.Write(message)
+			c.conn.WriteMessage(websocket.TextMessage, message)
 
-			n := len(c.send)
-			for i := 0; i < n; i++ {
-				w.Write(newline)
-				w.Write(<-c.send)
-			}
-
-			if err := w.Close(); err != nil {
-				return
-			}
 		case <-ticker.C:
 			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
 			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
