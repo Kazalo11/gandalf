eb86d641e75fc1d0b597eca7d09022c18f9d856d
Kazalo11 <kazoshodi@gmail.com>
2025-02-18 21:47:18 +0000

    actions

---
+++ server/actions.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"fmt"
+
+	"github.com/Kazalo11/gandalf/internals"
+	"github.com/Kazalo11/gandalf/models"
+	"github.com/google/uuid"
+)
+
+func processMessage(m Message, g *internals.Game) models.Card {
+	players := g.Players
+	playerId, err := uuid.Parse(m.PlayerId)
+	if err != nil {
+		fmt.Printf("%s is not a valid player Id", m.PlayerId)
+		return models.Card{}
+	}
+	player := players[playerId]
+
+	switch m.Action {
+	case DrawCard:
+		fmt.Println("Drawing card")
+		card := player.Draw(&g.Deck)
+		fmt.Printf("Card drawn is %v", card)
+		return card
+	case PlayCard:
+		fmt.Println("Playing card")
+	case Look:
+		fmt.Println("Looking at card")
+	case ShowCard:
+		fmt.Println("Showing card")
+	default:
+		fmt.Printf("%s is not a valid action", m.Action)
+		return models.Card{}
+	}
+	return models.Card{}
+}
